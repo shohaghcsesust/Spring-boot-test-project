ba74dc45422ac7fc5b5d89d7cbabe2802a2ae89b
ahsan.kabir <ahsan.kabir@bjitgroup.com>
2019-05-14 16:38:05 +0600

    Injecting external properties values into variables

--- src/main/java/com/shohagh/springboottest/controllers/HelloController.java
+++ src/main/java/com/shohagh/springboottest/controllers/HelloController.java
@@ -1,12 +1,16 @@
 package com.shohagh.springboottest.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+	@Value("${app.name}")
+	private String appName;
+
 	@RequestMapping("/sayHelloWorld")
 	public String printHello() {
-		return "Hello, World!";
+		return "Hello, World! " + appName;
 	}
 }
