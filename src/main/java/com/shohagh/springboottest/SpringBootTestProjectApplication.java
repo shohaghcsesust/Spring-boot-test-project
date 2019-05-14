6b8c0451cb38e62f94ce244bff9d097007319509
ahsan.kabir <ahsan.kabir@bjitgroup.com>
2019-05-14 16:21:16 +0600

    Configuring packages of all the given classes, including their sub-packages.

--- src/main/java/com/shohagh/springboottest/SpringBootTestProjectApplication.java
+++ src/main/java/com/shohagh/springboottest/SpringBootTestProjectApplication.java
@@ -3,7 +3,9 @@ package com.shohagh.springboottest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackageClasses = {
+		SpringBootTestProjectApplication.class,
+	    SomeClassInTheOtherPackage.class})
 public class SpringBootTestProjectApplication {
 
 	public static void main(String[] args) {
