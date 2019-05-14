652732340b0b97452b374d7dc6fce42a8cc304cb
ahsan.kabir <ahsan.kabir@bjitgroup.com>
2019-05-14 16:43:24 +0600

    DI using profiler

--- src/main/java/com/shohagh/springboottest/mail/MailConfig.java
+++ src/main/java/com/shohagh/springboottest/mail/MailConfig.java
@@ -2,15 +2,18 @@ package com.shohagh.springboottest.mail;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class MailConfig {
 	@Bean
+	@Profile("dev")
 	public MailSender mockMailSender() {
 		return new MockMailSender();
 	}
 
 	@Bean
+	@Profile("!dev")
 	public MailSender smtpMailSender() {
 		return new SmtpMailSender();
 	}
